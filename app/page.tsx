c88402cf1bb84042d47a8c7fb87ae23e145c12a1
Tarcísio Surdi <tarcisio.surdi@pm.me>
2023-11-04 19:21:25 -0300

    fix: mobile layout

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
             <Image
               src="/logo_server.jpg"
               alt="Logo HardSquad"
-              width={80}
+              width={60}
               height={24}
               priority
             />
@@ -28,7 +28,7 @@ export default function Home() {
       </div>
 
       <div className="relative flex place-items-center before:absolute before:h-[150px] before:w-[420px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <h1 className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] light:invert font-extrabold text-5xl">
+        <h1 className="text-center relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] light:invert font-extrabold text-5xl">
           Em construção...
         </h1>
       </div>
